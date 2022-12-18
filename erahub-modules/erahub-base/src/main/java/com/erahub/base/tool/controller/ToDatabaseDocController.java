656d97b05fce0606c5e6bd50273f6fe607e654b4
李彭 <15751839366@163.com>
2022-12-18 09:19:28 +0800

    publish 发布1.1.0正式版 版本号更新位置：所有xml,yml,package.json中的版本号， ToDatabaseDocController中的DOC_VERSION

--- erahub-modules/erahub-base/src/main/java/com/erahub/base/tool/controller/ToDatabaseDocController.java
+++ erahub-modules/erahub-base/src/main/java/com/erahub/base/tool/controller/ToDatabaseDocController.java
@@ -41,7 +41,7 @@ public class ToDatabaseDocController {
     private static final String FILE_OUTPUT_DIR = System.getProperty("java.io.tmpdir") + File.separator
         + "db-doc";
     private static final String DOC_FILE_NAME = "数据库文档";
-    private static final String DOC_VERSION = "1.0.0";
+    private static final String DOC_VERSION = "1.1.0";
     private static final String DOC_DESCRIPTION = "文档描述";
 
     @Log(title = "数据库文档", businessType = BusinessType.EXPORT)

955ca17d591b982924b8e2cb9ceaaea247f20959
李彭 <15751839366@163.com>
2023-01-02 09:37:21 +0800

    update 修改导出格式

--- erahub-modules/erahub-biz/src/main/java/com/erahub/biz/finance/domain/vo/BizFinanceERPProjectVo.java
+++ erahub-modules/erahub-biz/src/main/java/com/erahub/biz/finance/domain/vo/BizFinanceERPProjectVo.java
@@ -25,67 +25,67 @@ public class BizFinanceERPProjectVo {
     /**
      * ERP工程ID
      */
-    @ExcelProperty(value = "工程ID")
+    @ExcelProperty(value = "工程ID", index = 0)
     private Long projectId;
 
     /**
      * ERP工单号
      */
-    @ExcelProperty(value = "工单号")
+    @ExcelProperty(value = "工单号", index = 3)
     private String projectNumber;
 
     /**
      * ERP请购单号
      */
-    @ExcelProperty(value = "请购单号")
+    @ExcelProperty(value = "请购单号", index = 14)
     private String requisitionNumber;
 
     /**
      * ERP装置名称
      */
-    @ExcelProperty(value = "装置名称")
+    @ExcelProperty(value = "装置名称", index = 5)
     private String deviceName;
 
     /**
      * ERP工程名称
      */
-    @ExcelProperty(value = "工程名称")
+    @ExcelProperty(value = "工程名称", index = 6)
     private String projectName;
 
     /**
      * 计划类别
      */
-    @ExcelProperty(value = "计划类别")
+    @ExcelProperty(value = "计划类别", index = 7)
     private String planType;
 
     /**
      * 单位
      */
-    @ExcelProperty(value = "单位")
+    @ExcelProperty(value = "单位", index = 13)
     private String department;
 
     /**
      * 结算审核额
      */
-    @ExcelProperty(value = "结算审核额")
+    @ExcelProperty(value = "结算审核额", index = 8)
     private BigDecimal settlementReviewAmount;
 
     /**
      * 合同编号
      */
-    @ExcelProperty(value = "合同编号")
+    @ExcelProperty(value = "合同编号", index = 11)
     private String contractNumber;
 
     /**
      * 工程类别
      */
-    @ExcelProperty(value = "工程类别")
+//    @ExcelProperty(value = "工程类别")
     private String projectType;
 
     /**
      * 备注
      */
-    @ExcelProperty(value = "备注")
+//    @ExcelProperty(value = "备注")
     private String remark;
 
 

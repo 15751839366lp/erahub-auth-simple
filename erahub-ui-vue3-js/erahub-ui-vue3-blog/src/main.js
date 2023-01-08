0cbc49d777269e34927151e1a56531cb37000690
李彭 <15751839366@163.com>
2023-01-08 10:08:50 +0800

    rollback blog前端回退

--- erahub-ui-vue3-js/erahub-ui-vue3-blog/src/main.js
+++ erahub-ui-vue3-js/erahub-ui-vue3-blog/src/main.js
@@ -1,44 +1,70 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './router/guard'
-import '@/styles/index.scss'
-import 'normalize.css/normalize.css'
-import { createPinia } from 'pinia'
-import { i18n } from './locales'
-import VueClickAway from 'vue3-click-away'
-import lazyPlugin from 'vue3-lazy'
-import { registerSvgIcon } from '@/icons'
-import { registerObSkeleton } from '@/components/LoadingSkeleton'
-import 'prismjs/themes/prism.css'
-import 'prismjs'
-import 'element-plus/theme-chalk/index.css'
-import { components, plugins } from './plugins/element-plus'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import infiniteScroll from 'vue3-infinite-scroll-better'
-import v3ImgPreview from 'v3-img-preview'
-import api from './api/api'
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-export const app = createApp(App)
-  .use(router)
-  .use(pinia)
-  .use(i18n)
-  .use(VueClickAway)
-  .use(infiniteScroll)
-  .use(v3ImgPreview, {})
-  .use(lazyPlugin, {
-    loading: require('@/assets/default-cover.jpg'),
-    error: require('@/assets/default-cover.jpg')
-  })
-components.forEach((component) => {
-  app.component(component.name, component)
+import Cookies from 'js-cookie'
+
+import ElementPlus from 'element-plus'
+import locale from 'element-plus/lib/locale/lang/zh-cn' // 中文语言
+
+import '@/assets/styles/index.scss' // global css
+
+// 引入全局样式
+import '@/styles/index.less'
+
+import App from './App'
+import store from './store'
+import router from './router'
+import directive from './directive' // directive
+
+import 'virtual:windi.css'
+import 'virtual:windi-devtools' // windicss
+
+// 注册指令
+import plugins from './plugins' // plugins
+import { download } from '@/utils/request'
+
+// 图标
+import 'virtual:svg-icons-register'
+
+import './permission' // permission control
+
+import { useDict } from '@/utils/dict'
+
+import {
+  parseTime,
+  resetForm,
+  addDateRange,
+  handleTree,
+  selectDictLabel,
+  selectDictLabels
+} from '@/utils/erahub'
+
+const app = createApp(App)
+
+// 全局方法挂载
+app.config.globalProperties.useDict = useDict
+app.config.globalProperties.download = download
+app.config.globalProperties.parseTime = parseTime
+app.config.globalProperties.resetForm = resetForm
+app.config.globalProperties.handleTree = handleTree
+app.config.globalProperties.addDateRange = addDateRange
+app.config.globalProperties.selectDictLabel = selectDictLabel
+app.config.globalProperties.selectDictLabels = selectDictLabels
+
+app.use(router)
+app.use(store)
+app.use(plugins)
+app.use(elementIcons)
+
+directive(app)
+
+// 使用element-plus 并且设置全局的大小
+app.use(ElementPlus, {
+  locale: locale,
+  // 支持 large、default、small
+  size: Cookies.get('size') || 'default'
 })
-plugins.forEach((plugin) => {
-  app.use(plugin)
-})
-registerSvgIcon(app)
-registerObSkeleton(app)
+
+// 修改 el-dialog 默认点击遮照为不关闭
+app._context.components.ElDialog.props.closeOnClickModal.default = false
+
 app.mount('#app')
-api.report()
